a23b1ecd8e30f4fa46c59d9f60118212dcd3061b
ssnover <ssnover95@gmail.com>
2022-02-21 23:06:53 -0700

    Cargo fmt

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,15 +10,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let (connection, handle, _) = rtnetlink::new_connection()?;
     tokio::spawn(connection);
     add_address(if_name, Ipv4Addr::new(10, 0, 0, 1).into(), handle).await?;
-    
+
     netty.run().await?;
     Ok(())
 }
 
-async fn add_address(if_name: &str, addr: IpAddr, handle: rtnetlink::Handle) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let mut links = handle.link().get().match_name(if_name.to_string()).execute();
+async fn add_address(
+    if_name: &str,
+    addr: IpAddr,
+    handle: rtnetlink::Handle,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let mut links = handle
+        .link()
+        .get()
+        .match_name(if_name.to_string())
+        .execute();
     if let Some(link) = links.try_next().await? {
-        handle.address().add(link.header.index, addr, 24).execute().await?;
+        handle
+            .address()
+            .add(link.header.index, addr, 24)
+            .execute()
+            .await?;
     }
     Ok(())
 }
